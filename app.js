e12489a6c5f53e49ac033487eaccf38198e45241
odenkun <mykoic@yahoo.co.jp>
2017-12-06 01:53:59 +0900

    njkl

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
 const http = require("http");
 const WebSocketServer = require('websocket').server;
 
-const server = http.createServer(function(request, response) {
+const server = http.createServer(function (request, response) {
 	console.log((new Date()) + ' Received request for ' + request.url);
 	response.writeHead(404);
 	response.end();
 });
-server.listen(80, function() {
-	console.log((new Date()) + ' Server is listening on port 8080');
+const port = 80;
+server.listen(port, function () {
+	console.log((new Date()) + ' Server is listening on port ' + port);
 });
 
 const wsServer = new WebSocketServer({
@@ -22,7 +23,38 @@ function originIsAllowed(origin) {
 	return true;
 }
 
-wsServer.on('request', (request) =>  {
+const speech = require('@google-cloud/speech');
+const auth = {
+	projectId: 'yutaroproject-185304',
+	keyFilename: 'auth/google.json'
+};
+
+const requestConf = {
+	config: {
+		encoding: 'LINEAR16',
+		sampleRateHertz: 16000,
+		languageCode: 'ja-JP',
+	},
+	interimResults: false,
+};
+
+
+const recognizeConnections = {};
+wsServer.on('request', (request) => {
+	if (request.remoteAddress) {
+		console.log("client created");
+		const client = new speech.SpeechClient(auth);
+		recognizeConnections.remoteAddress = client
+			.streamingRecognize(requestConf)
+			.on('error', error => console.error(error))
+			.on('data', data => {
+				console.log("received data");
+				console.log(data);
+			});
+		recognizeConnections.remoteAddress.write();
+	} else {
+		request.reject();
+	}
 	//アクセスが許されない場合
 	if (!originIsAllowed(request.origin)) {
 		//アクセス拒否
@@ -30,51 +62,26 @@ wsServer.on('request', (request) =>  {
 		console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
 		return;
 	}
-
-	const connection = request.accept('echo-protocol', request.origin);
-	console.log((new Date()) + ' Connection accepted.');
+	console.log((new Date()) + ' Connection accepted.' + request.origin);
+	//サブプロトコル
+	const connection = request.accept('recognize', request.origin);
 	connection.on('message', (message) => {
-		if (message.type === 'utf8') {
-			console.log('Received Message: ' + message.utf8Data);
-			connection.sendUTF(message.utf8Data);
-		}
-		else if (message.type === 'binary') {
+		if (message.type === ' utf8') {
+			console.log('Received utf8 Message of ' + message.utf8Data);
+			if (message.utf8Data === 'stopRecognize') {
+				console.log('Stop recognize');
+				recognizeConnections.remoteAddress.end();
+			}
+		} else if (message.type === 'binary') {
 			console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-			connection.sendBytes(message.binaryData);
+			recognizeConnections.remoteAddress.write(
+				{
+					audio_content: message.binaryData
+				}
+			);
 		}
-	});
-	connection.on('close', (reasonCode, description) => {
+	}).on('close', (reasonCode, description) => {
 		console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+		recognizeConnections.remoteAddress.end();
 	});
 });
-
-
-
-function streamingRecognize() {
-	const speech = require('@google-cloud/speech');
-	// Creates a client
-	const client = new speech.SpeechClient();
-	const encoding = 'LINEAR16';
-	const sampleRateHertz = 16000;
-	const languageCode = 'ja-JP';
-
-	const request = {
-		config: {
-			encoding: encoding,
-			sampleRateHertz: sampleRateHertz,
-			languageCode: languageCode,
-		},
-		interimResults: false,
-	};
-
-	// Stream the audio to the Google Cloud Speech API
-	const recognizeStream = client
-		.streamingRecognize(request)
-		.on('error', console.error)
-		.on('data', data => {
-			const transcript = data.results[0].alternatives[0].transcript;
-			console.log(`Transcription: ${transcript}`);
-		});
-
-	// pipe(recognizeStream);
-}
